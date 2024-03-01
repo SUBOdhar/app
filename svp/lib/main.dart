9b04deb879820af27e78a4c508df306f3d8e0229
SUBOdhar <aryalsubodh4@ail.com>
2024-03-01 20:32:10 +0545

    auto login if once logined for 17 days

--- svp/lib/main.dart
+++ svp/lib/main.dart
@@ -1,20 +1,44 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import 'second_page.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData.light(),
+      home: const HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    // Call _login function when the page is initialized
+    _login();
+  }
+
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FocusNode _passwordFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode =
+      FocusNode(); // FocusNode for password field
   bool _loading = false;
   Color _buttonColor = const Color.fromRGBO(143, 148, 251, 1);
   bool _obscureText = true;
@@ -25,113 +49,6 @@ class _HomePageState extends State<HomePage> {
     _passwordController.dispose();
     _passwordFocusNode.dispose();
     super.dispose();
-  }
-
-  Future<void> _login() async {
-    if (!_loading) {
-      setState(() {
-        _loading = true;
-        _buttonColor = Colors.grey;
-      });
-
-      String email = _emailController.text;
-      String password = _passwordController.text;
-
-      if (email.isEmpty || password.isEmpty) {
-        // Check if stored credentials exist
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        String storedEmail = prefs.getString('email') ?? '';
-        String storedPassword = prefs.getString('password') ?? '';
-        int storedTimestamp = prefs.getInt('timestamp') ?? 0;
-
-        if (storedEmail.isNotEmpty && storedPassword.isNotEmpty) {
-          // Check if stored credentials are expired
-          DateTime storedDateTime =
-              DateTime.fromMillisecondsSinceEpoch(storedTimestamp);
-          DateTime now = DateTime.now();
-          final difference = now.difference(storedDateTime).inDays;
-          if (difference <= 13) {
-            // Use stored credentials if not expired
-            email = storedEmail;
-            password = storedPassword;
-          } else {
-            // Remove expired credentials
-            await prefs.remove('email');
-            await prefs.remove('password');
-            await prefs.remove('timestamp');
-          }
-        } else {
-          // If no stored credentials, show error message
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text('Please enter email and password.'),
-            backgroundColor: Colors.red,
-          ));
-          setState(() {
-            _loading = false;
-            _buttonColor = const Color.fromRGBO(143, 148, 251, 1);
-          });
-          return;
-        }
-      }
-
-      Map<String, dynamic> data = {
-        'email': email,
-        'password': password,
-        'app':'svp',
-      };
-
-      String jsonData = jsonEncode(data);
-
-      try {
-        http.Response response = await http.post(
-          Uri.parse('https://api.svp.com.np/login'),
-          headers: <String, String>{
-            'Content-Type': 'application/json',
-          },
-          body: jsonData,
-        );
-
-        if (response.statusCode == 200) {
-          // Login successful
-          Map<String, dynamic> responseData = jsonDecode(response.body);
-          print('Response body: $responseData');
-          print('Login successful!');
-
-          // Save email, password, and timestamp
-          SharedPreferences prefs = await SharedPreferences.getInstance();
-          prefs.setString('email', email);
-          prefs.setString('password', password);
-          prefs.setInt('timestamp', DateTime.now().millisecondsSinceEpoch);
-
-          // Navigate to the SecondPage
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const SecondPage()),
-          );
-        } else {
-          // Login failed
-          print('Error: ${response.statusCode}');
-          // Show an error message to the user
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: Text('Login failed. Please try again.'),
-            backgroundColor: Colors.red,
-          ));
-        }
-      } catch (error) {
-        // Error occurred during login request
-        print('Error: $error');
-        // Show an error message to the user
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('An error occurred. Please try again later.'),
-          backgroundColor: Colors.red,
-        ));
-      }
-
-      setState(() {
-        _loading = false;
-        _buttonColor = const Color.fromRGBO(143, 148, 251, 1);
-      });
-    }
   }
 
   @override
@@ -212,7 +129,7 @@ class _HomePageState extends State<HomePage> {
                             ),
                             child: TextField(
                               controller: _emailController,
-                              autofillHints: [AutofillHints.email],
+                              autofillHints: const [AutofillHints.email],
                               onSubmitted: (_) => FocusScope.of(context)
                                   .requestFocus(_passwordFocusNode),
                               style: const TextStyle(
@@ -255,8 +172,8 @@ class _HomePageState extends State<HomePage> {
                             ),
                             child: TextField(
                               controller: _passwordController,
+                              autofillHints: const [AutofillHints.password],
                               focusNode: _passwordFocusNode,
-                              autofillHints: [AutofillHints.password],
                               obscureText: _obscureText,
                               style: const TextStyle(
                                 color: Colors.black,
@@ -320,5 +237,107 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  void _login() async {
+    if (!_loading) {
+      setState(() {
+        _loading = true;
+        _buttonColor = Colors.grey;
+      });
+
+      String email = _emailController.text;
+      String password = _passwordController.text;
+
+      if (email.isEmpty || password.isEmpty) {
+        // Check if stored credentials exist
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        String storedEmail = prefs.getString('email') ?? '';
+        String storedPassword = prefs.getString('password') ?? '';
+        int storedTimestamp = prefs.getInt('timestamp') ?? 0;
+
+        if (storedEmail.isNotEmpty && storedPassword.isNotEmpty) {
+          // Check if stored credentials are expired
+          DateTime storedDateTime =
+              DateTime.fromMillisecondsSinceEpoch(storedTimestamp);
+          DateTime now = DateTime.now();
+          final difference = now.difference(storedDateTime).inDays;
+          if (difference <= 13) {
+            // Use stored credentials if not expired
+            email = storedEmail;
+            password = storedPassword;
+          } else {
+            // Remove expired credentials
+            prefs.remove('email');
+            prefs.remove('password');
+            prefs.remove('timestamp');
+          }
+        } else {
+          // If no stored credentials, return without displaying an error
+          setState(() {
+            _loading = false;
+            _buttonColor = const Color.fromRGBO(143, 148, 251, 1);
+          });
+          return;
+        }
+      }
+
+      Map<String, dynamic> data = {
+        'email': email,
+        'password': password,
+      };
+
+      String jsonData = jsonEncode(data);
+
+      try {
+        http.Response response = await http.post(
+          Uri.parse('https://api.svp.com.np/login'),
+          headers: <String, String>{
+            'Content-Type': 'application/json',
+          },
+          body: jsonData,
+        );
+
+        if (response.statusCode == 200) {
+          // Login successful
+          Map<String, dynamic> responseData = jsonDecode(response.body);
+          print('Response body: $responseData');
+          print('Login successful!');
+
+          // Save email, password, and timestamp
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          prefs.setString('email', email);
+          prefs.setString('password', password);
+          prefs.setInt('timestamp', DateTime.now().millisecondsSinceEpoch);
+
+          // Navigate to the SecondPage
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SecondPage()),
+          );
+        } else {
+          // Login failed
+          print('Error: ${response.statusCode}');
+          // Show an error message to the user
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Login failed. Please try again.'),
+            backgroundColor: Colors.red,
+          ));
+        }
+      } catch (error) {
+        // Error occurred during login request
+        print('Error: $error');
+        // Show an error message to the user
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('An error occurred. Please try again later.'),
+          backgroundColor: Colors.red,
+        ));
+      }
+
+      setState(() {
+        _loading = false;
+        _buttonColor = const Color.fromRGBO(143, 148, 251, 1);
+      });
+    }
   }
 }
